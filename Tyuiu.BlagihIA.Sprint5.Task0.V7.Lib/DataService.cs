c8a07e77f81c7a00549a3ba22eac71bfdba6e650
Игорь Благих <Tyuiu.Blagihia@outlook.com>
2024-11-25 21:36:02 +0500

    Task0

--- Tyuiu.BlagihIA.Sprint5.Task0.V7.Lib/DataService.cs
+++ Tyuiu.BlagihIA.Sprint5.Task0.V7.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.BlagihIA.Sprint5.Task0.V7.Lib
         {
             string path = Path.GetTempFileName();
             x = 4;
-            double res = -4 ^ 3 + 4 * 4 ^ 2 - (3 / 2) * 4;
+            double res = (-4 ^ 3) + (4 * 4 ^ 2) - ((3 / 2) * 4);
             res = Math.Round(res);
             File.AppendAllText(path, x.ToString());
             return path;
