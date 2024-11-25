aed48ff384e46e9c443f269f2854303aec98c56f
Игорь Благих <Tyuiu.Blagihia@outlook.com>
2024-11-25 21:49:35 +0500

    Task0

--- Tyuiu.BlagihIA.Sprint5.Task0.V7.Lib/DataService.cs
+++ Tyuiu.BlagihIA.Sprint5.Task0.V7.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.BlagihIA.Sprint5.Task0.V7.Lib
         {
             string path = "OutPutFileTask0.txt";
             x = 4;
-            double res = Math.Pow(x,3) + 4 * Math.Pow(x,2) - (3d/2d *4);
+            double res = Math.Pow(x,3) + 4 * Math.Pow(x,2) - (3.0/2.0 *4);
             res = Math.Round(res);
             File.AppendAllText(path, x.ToString());
             return path;
