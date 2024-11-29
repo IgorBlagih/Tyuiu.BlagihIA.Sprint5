e07d45ba7a95424f087bbbab7a302f95224ee667
Игорь Благих <Tyuiu.Blagihia@outlook.com>
2024-11-29 20:05:52 +0500

    Task5

--- Tyuiu.BlagihIA.Sprint5.Task5.V18.Lib/DataService.cs
+++ Tyuiu.BlagihIA.Sprint5.Task5.V18.Lib/DataService.cs
@@ -7,17 +7,10 @@ namespace Tyuiu.BlagihIA.Sprint5.Task5.V18.Lib
         public double LoadFromDataFile(string path)
         {
             double res = 1;
-            using(StreamReader reader = new StreamReader(path))
-            {
-                string line ;
-                
-                while ((line = reader.ReadLine()) != null)
-                {
-                    line.Replace(".", ",");
-                    res = res * Math.Round(Convert.ToDouble(line), 3);
-
-                }
-            }
+            string strx = File.ReadAllText(path);
+            strx = strx.Replace(".", ",");
+            double x = Convert.ToDouble(strx);
+            res = res* x;
             return res;
         }
     }
